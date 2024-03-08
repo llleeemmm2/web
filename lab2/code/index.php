04937995e51f8f1a7e26d83d76286f1caf3a1675
llleeemmm2 <miroshnik2212@mail.ru>
2024-03-08 22:39:18 +0200

    Complete task2

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -11,3 +11,20 @@ echo "\nYour order is: $very_bad_unclear_name.";
 
 echo "<br>";
 
+// Task 2
+$num1 = 20;
+echo $num1;
+echo "\n";
+
+$num2 = 22;
+echo $num2;
+echo "\n";
+
+$num3 = 22.12;
+echo $num3;
+echo "\n";
+
+$num4 = 4;
+$num5 = 8;
+echo $num4 + $num5;
+
